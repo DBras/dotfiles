87fa1c1ac26039ff439bd6588dfef74e29a2f168
McDannel <danielbrasholt@gmail.com>
2022-01-10 19:12:36 +0100

    Reinstate MOD+ctrl+H functionality

--- .config/qtile/keys.py
+++ .config/qtile/keys.py
@@ -28,9 +28,9 @@ keys = [
 
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-#    Key([mod, "shift"], "h",
-#        lazy.layout.shuffle_left(),
-#        desc="Move window to the left"),
+    Key([mod, "shift"], "h",
+        lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
     Key([mod, "shift"], "l",
         lazy.layout.shuffle_right(),
         desc="Move window to the right"),
@@ -43,9 +43,9 @@ keys = [
 
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-#    Key([mod, "control"], "h",
-#        lazy.layout.grow_left(),
-#        desc="Grow window to the left"),
+    Key([mod, "control"], "h",
+        lazy.layout.grow_left(),
+        desc="Grow window to the left"),
     Key([mod, "control"], "l",
         lazy.layout.grow_right(),
         desc="Grow window to the right"),
